a2a22e7fa6a190b3bd6900b02d2df29295d05dbc
NeuralScribe <dasanamika372@gmail.com>
2025-02-01 23:02:04 +0530

    Prog to check a given num is palindrom or not

--- Python/NumberSystem/Palindrom.py
+++ Python/NumberSystem/Palindrom.py
@@ -1,1 +1,11 @@
-print("hello world")
+num = int(input("Enter the number: "))
+temp = num
+rev = 0
+while(num>0):
+    dig = num % 10
+    rev = rev *10 + dig
+    num = num // 10  
+if(temp == rev):  
+    print("This number is a palindrome number!")  
+else:  
+    print("This number is not a palindrome number!")  
