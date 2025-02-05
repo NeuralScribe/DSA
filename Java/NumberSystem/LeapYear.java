3ddb8f1dbebd5e366e3cc2960b977e627b24cace
NeuralScribe <dasanamika372@gmail.com>
2025-02-05 22:58:00 +0530

    Prog to check a year is leapyear or not.

---
+++ Java/NumberSystem/LeapYear.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class LeapYear {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the year :");
+        int year = sc.nextInt();
+        if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0))
+        {
+            System.err.println("The year is leapyear!");
+        }
+        else
+        {
+            System.err.println("The year is not leapyear!");
+        }
+
+
+          
+    }
+
+}
