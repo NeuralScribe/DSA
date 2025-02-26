cc6df313ad9e63e4a1abfc268ff63d4eff006f9b
NeuralScribe <dasanamika372@gmail.com>
2025-02-27 01:11:56 +0530

    Program to Reverse An Array

---
+++ Python/Array/ReverseAnArray.py
@@ -0,0 +1,10 @@
+def swap(arr, a,b):
+    arr[a] , arr[b] = arr[b] , arr[a]
+arr = list(map(int,input().strip().split()))
+left , right = 0, len(arr) -1
+while left < right :
+    swap (arr, left, right)
+    left += 1
+    right -=1
+for num in arr:
+        print(num)
