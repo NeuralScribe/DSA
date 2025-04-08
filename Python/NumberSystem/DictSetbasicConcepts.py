5e4f8b2563e98fed3cab377ea4c83275f05deec8
NeuralScribe <dasanamika372@gmail.com>
2025-04-08 17:34:10 +0530

    basic concepts

---
+++ Python/NumberSystem/DictSetbasicConcepts.py
@@ -0,0 +1,34 @@
+# Java te Hasmap korto orom map ekhane - Dictionary
+# Java te Hashset jeta korto ota ekhane - Set
+# Java te jeta arraylist korto ota ekhane array
+
+# Dictionary ta key value pair e store kore
+# Set ta unique element store kore
+
+# Find the frequency of elements in an array - > Dictionary
+
+[1, 2, 2, 1, 3, 8, 8, 9]
+
+# 1->2
+# 2->2
+# 3->1
+# 8->2
+# 9->1
+
+# Find the unique elements of the array -> Set
+
+[1, 2, 2, 1, 3, 8, 8, 9]
+
+# 1,2,3,8,9
+
+
+# An array has been given in input -> You need to do some certain operation such that after finding the square of every element you need to print the total sum of them
+
+[1, 2, 2, 1, 3, 8, 8, 9]
+
+# 1+4+4+1+9+64+64+81
+
+
+arr = list(map(int, input().strip().split()))
+
+# How to take input of array in python
