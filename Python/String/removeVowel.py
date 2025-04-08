aea6580a9f8569a9987822a893bd52980b2bae23
NeuralScribe <dasanamika372@gmail.com>
2025-04-09 00:12:40 +0530

    python code

--- Python/String/removeVowel.py
+++ Python/String/removeVowel.py
@@ -17,3 +17,19 @@ for a in str:
     else:
         newStr = newStr+a
 print(newStr)
+
+# for a in str:      .....code to remove the consonent......
+#     if(
+#         a=="a"
+#     or  a=="e"
+#     or  a=="i"
+#     or  a=="o"
+#     or  a=="u"
+#     or  a=="A"
+#     or  a=="E"
+#     or  a=="I"
+#     or  a=="O"
+#     or  a=="U"
+#     ):
+#        newStr = newStr+a
+# print(newStr)
